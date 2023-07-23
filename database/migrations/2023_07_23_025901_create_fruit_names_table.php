9fea9ac9c7971ba0fface6e45ed95e376ba926bd
KYOJINNAAPYON <kyojinnaapyon@yahoo.co.jp>
2023-07-23 12:22:31 +0900

    nameカラム追加

--- database/migrations/2023_07_23_025901_create_fruit_names_table.php
+++ database/migrations/2023_07_23_025901_create_fruit_names_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('fruit_names', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
